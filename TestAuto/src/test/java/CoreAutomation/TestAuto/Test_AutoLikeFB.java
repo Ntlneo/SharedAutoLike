636e0c4677c8c49b461dc2c014cca22bbefecac3
Nguyen Thanh Lam <lam.nguyenthanh84@gmail.com>
2020-06-06 02:35:10 +0700

    fix major bugs

--- TestAuto/src/test/java/CoreAutomation/TestAuto/Test_AutoLikeFB.java
+++ TestAuto/src/test/java/CoreAutomation/TestAuto/Test_AutoLikeFB.java
@@ -1,16 +1,12 @@
 package CoreAutomation.TestAuto;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,9 +25,11 @@ public class Test_AutoLikeFB {
 	// changed	
 	static String username = "ntlneo";
 	static String password = "Docnhat001@";
+//	static String emailFB = "an.thanh282000@gmail.com";
+//	static String passFB = "Docnhat001@";
 	static String emailFB = "suzukihzt@gmail.com";
 	static String passFB = "Docnhat1";
-	static int numberOfLoop = 20;
+	static int numberOfLoop = 60;
 
 	// locators
 	static By loginBtn = By.xpath("//a[contains(@title,'Login')]");
@@ -46,11 +44,11 @@ public class Test_AutoLikeFB {
 	static By listLikeBtn = By.xpath("//td[contains(@id,'task')]");
 
 	// new window with pagelike
-	static By dangnhapBtn = By.xpath("(//*[text()='Đăng nhập'])[1]/..");
+	static By dangnhapBtn = By.xpath("(//*[contains(text(),'Đăng nhập') or contains(text(),'Log in') or contains(text(),'Login')]/ancestor::a)[1]");
 	static By emailFbBox = By.xpath("//*[@name='email']");
 	static By passFbBox = By.xpath("//*[@name='pass']");
 	static By loginFbBtn = By.xpath("//*[@name='login']");
-	static By likePageBtn = By.xpath("//*[@aria-label='like button' or @aria-label='nút thích']");
+	static By likePageBtn = By.xpath("//*[@aria-label='like button' or @aria-label='Like button' or @aria-label='nút thích' or @aria-label='Nút thích']");
 	static By likePostBtn = By.xpath("//a[@data-autoid='autoid_7']");
 
 	@org.junit.Test
@@ -78,23 +76,32 @@ public class Test_AutoLikeFB {
 	}
 
 	// *********************** MIX ***********************
-
+	
+	static void checkBonusPage() {
+		String currentURL = driver.getCurrentUrl();
+		if (currentURL.contains("bonus-page")) {
+			System.out.println("***** STOP AUTO-LIKE since \'Active members Bonus\' page displayed *****");
+			Set<String> windows = driver.getWindowHandles();
+			for (String window : windows) {
+				driver.switchTo().window(window);
+				driver.close();
+			}			
+			fail("\n***** STOP AUTO-LIKE since \'Active members Bonus\' page's displayed *****");
+		}
+	}
+	
 	static void doLoopLike() {
 		int count = 0;
 		int numberOfLikeBtn = getListWebElement(listLikeBtn).size();
 		
 			for (int i = 0; i < numberOfLikeBtn; i++) {
 				if (count < numberOfLoop) {
-				String currentURL = driver.getCurrentUrl();
-				if (currentURL.contains("bonus-page")) {
-					System.out.println("***** STOP AUTO-LIKE since \'Active members Bonus\' page displayed *****");
-					driver.quit();
-					fail("***** STOP AUTO-LIKE since \'Active members Bonus\' page's displayed *****");
-				}
+				checkBonusPage();
 				String strSearch = "')]/descendant";
 				String strNewLikeBtn = strLikeBtn.replace(strSearch, i + strSearch);
 				By NewLikeBtn = By.xpath(strNewLikeBtn);
 				click(NewLikeBtn);
+				checkBonusPage();
 				String firstWindow = driver.getWindowHandle();
 				Set<String> windows = driver.getWindowHandles();
 				for (String window : windows) {
@@ -106,8 +113,12 @@ public class Test_AutoLikeFB {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				if (i == 0 && count == 0) {
-					click(dangnhapBtn);
+				if (i == 0 && count == 0) {					
+					try{
+						click(dangnhapBtn);
+					}catch (Exception e) {
+						click(dangnhapBtn);						
+					}
 					input(emailFbBox, emailFB);
 					input(passFbBox, passFB);
 					click(loginFbBtn);
@@ -148,12 +159,7 @@ public class Test_AutoLikeFB {
 
 	static void openFbLike() {
 		hoverAndClick(smExchangeBtn, fbLikesBtn);
-		String currentURL = driver.getCurrentUrl();
-		if (currentURL.contains("bonus-page")) {
-			System.out.println("***** STOP AUTO-LIKE since \'Active members Bonus\' page displayed *****");
-			driver.quit();
-			fail("***** STOP AUTO-LIKE since \'Active members Bonus\' page displayed *****");
-		}
+		checkBonusPage();
 	}
 
 	static void doLogin() {
@@ -168,14 +174,15 @@ public class Test_AutoLikeFB {
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		System.setProperty("webdriver.chrome.silentOutput", "true");
 		ChromeOptions options = new ChromeOptions();
-		options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation", "enable-logging" });
+//		options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation", "enable-logging" });
+		options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
 		options.setExperimentalOption("useAutomationExtension", false);
 		Map<String, Object> prefs = new HashMap<String, Object>();
 		prefs.put("credentials_enable_service", false);
 		prefs.put("profile.password_manager_enabled", false);
 		options.setExperimentalOption("prefs", prefs);
 		driver = new ChromeDriver(options);
-		driver.manage().window().setPosition(new Point(0, 50000));
+//		driver.manage().window().setPosition(new Point(0, 50000));
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
